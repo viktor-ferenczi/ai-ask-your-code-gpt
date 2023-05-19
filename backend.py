152fa9d2e5d2df8a76eb16cb412a84e32bb0c014
Viktor Ferenczi <viktor@ferenczi.eu>
2023-05-19 18:09:33 +0200

    Plugin manifest, API descriptor, skeleton server

---
+++ backend.py
@@ -0,0 +1,17 @@
+import uuid
+from typing import List
+
+
+def download(username: str, url: str) -> str:
+    # TODO
+    return str(uuid.uuid4())
+
+
+def delete(username, project_id):
+    # TODO
+    pass
+
+
+def search(username: str, url: str) -> List[str]:
+    # TODO
+    return []
--- main.py
+++ main.py
@@ -1,25 +1,107 @@
 import json
+import sys
+from traceback import print_exc
+from typing import Dict
 
 import quart
 import quart_cors
-from quart import request
+
+import backend
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
-# Keep track of todo's. Does not persist if Python session is restarted.
 _TODOS = {}
 
-@app.post("/todos/<string:username>")
-async def add_todo(username):
-    request = await quart.request.get_json(force=True)
-    if username not in _TODOS:
-        _TODOS[username] = []
-    _TODOS[username].append(request["todo"])
+DEVELOPMENT = sys.platform == 'win32'
+
+
+def html_prod_to_dev(text):
+    text = text.replace('https://askyourcode.ai', 'http://127.0.0.1:5003')
+    return text
+
+
+@app.get("/")
+async def index():
+    return await quart.send_file('index.html', mimetype='text/html')
+
+
+@app.get("/logo.png")
+async def logo():
+    return await quart.send_file('logo.png', mimetype='image/png')
+
+
+@app.get("/.well-known/ai-plugin.json")
+async def plugin_manifest():
+    with open("ai-plugin.json") as f:
+        text = f.read()
+    if DEVELOPMENT:
+        text = html_prod_to_dev(text)
+    return quart.Response(text, mimetype="text/json")
+
+
+@app.get("/openapi.yaml")
+async def openapi_spec():
+    with open("openapi.yaml") as f:
+        text = f.read()
+    if DEVELOPMENT:
+        text = html_prod_to_dev(text)
+    return quart.Response(text, mimetype="text/yaml")
+
+
+@app.post("/project/<string:username>")
+async def download(username: str):
+    if not username:
+        return quart.Response(response='Missing username', status=400)
+
+    request: Dict[str, str] = await quart.request.get_json(force=True)
+    url: str = request.get('url')
+    if not url:
+        return quart.Response(response='Missing url')
+    if not (url.startswith('http://') or url.startswith('https://')):
+        return quart.Response(response='The URL must start with http:// or https://', status=400)
+
+    # noinspection PyBroadException
+    try:
+        project_id = backend.download(username, url)
+    except KeyboardInterrupt:
+        raise
+    except Exception:
+        print(f'ERROR: User {username!r} failed to download source archive {url!r}:')
+        print_exc()
+        return quart.Response(response='Failed to download or store the source archive', status=400)
+
+    response = {
+        'project_id': project_id
+    }
+    return quart.Response(response=json.dumps(response, indent=2), status=200)
+
+
+@app.delete("/project/<string:username>/<string:project_id>")
+async def delete(username: str, project_id: str):
+    if not username:
+        return quart.Response(response='Missing username', status=400)
+
+    backend.delete(username, project_id)
     return quart.Response(response='OK', status=200)
 
-@app.get("/todos/<string:username>")
-async def get_todos(username):
-    return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
+
+@app.get("/project/<string:username>/<string:project_id>/search")
+async def search(username: str, project_id: str):
+    if not username:
+        return quart.Response(response='ERROR: Missing username', status=400)
+
+    # noinspection PyBroadException
+    try:
+        results = backend.search(username, project_id)
+    except KeyboardInterrupt:
+        raise
+    except Exception:
+        print(f'ERROR: User {username!r} failed to search project {project_id!r}:')
+        print_exc()
+        return quart.Response(response='ERROR: URL must start with http:// or https://', status=400)
+
+    return quart.Response(response=json.dumps(results, indent=2), status=200)
+
 
 @app.delete("/todos/<string:username>")
 async def delete_todo(username):
@@ -30,27 +112,10 @@ async def delete_todo(username):
         _TODOS[username].pop(todo_idx)
     return quart.Response(response='OK', status=200)
 
-@app.get("/logo.png")
-async def plugin_logo():
-    filename = 'logo.png'
-    return await quart.send_file(filename, mimetype='image/png')
-
-@app.get("/.well-known/ai-plugin.json")
-async def plugin_manifest():
-    host = request.headers['Host']
-    with open("./.well-known/ai-plugin.json") as f:
-        text = f.read()
-        return quart.Response(text, mimetype="text/json")
-
-@app.get("/openapi.yaml")
-async def openapi_spec():
-    host = request.headers['Host']
-    with open("openapi.yaml") as f:
-        text = f.read()
-        return quart.Response(text, mimetype="text/yaml")
 
 def main():
-    app.run(debug=True, host="0.0.0.0", port=5003)
+    app.run(debug=True, host="127.0.0.1", port=5003)
+
 
 if __name__ == "__main__":
     main()
