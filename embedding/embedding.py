29713f7b8e856c2eb558ec1e1a4c60596aad4260
Viktor Ferenczi <viktor@ferenczi.eu>
2023-05-21 14:56:04 +0200

    Refactored the embedding code

---
+++ embedding/embedding.py
@@ -0,0 +1,41 @@
+from typing import List
+
+import numpy as np
+import torch
+
+import doc_types
+from doc_types import TextDocType
+from model.fragment import Fragment
+
+
+class Embedding:
+    # XL: 7.0 ms/embedding, 6.2GB GPU RAM usage
+    # model_name = 'hkunlp/instructor-xl'
+
+    # XL: 2.2 ms/embedding, 3GB GPU RAM usage
+    model_name = 'hkunlp/instructor-large'
+
+    def __init__(self) -> None:
+        if not torch.cuda.is_available():
+            raise EnvironmentError('CUDA is not available')
+
+        # TODO: Cite them properly!
+        from InstructorEmbedding import INSTRUCTOR
+        self.model = INSTRUCTOR(self.model_name)
+        self.model.to('cuda')
+
+    def embed_fragments(self, fragments: List[Fragment]) -> np.numarray:
+        # noinspection PyTypeChecker
+        embeddings = self.model.encode([[doc_types.detect_by_extension(fragment.path).store_instruction + ':', fragment.text] for fragment in fragments])
+        return embeddings
+
+    def embed_query(self, text: str) -> np.numarray:
+        if text.startswith('.'):
+            extension = text.split(' ')[0].lower()[1:]
+            instruction = doc_types.detect_by_extension(extension).query_instruction
+        else:
+            instruction = TextDocType.query_instruction
+
+        # noinspection PyTypeChecker
+        embeddings = self.model.encode([[instruction + ':', text]])
+        return embeddings
---
+++ embedding/test_embedding.py
@@ -0,0 +1,66 @@
+import unittest
+
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+from model.fragment import Fragment
+from .embedding import Embedding
+
+
+class TestEmbedding(unittest.TestCase):
+
+    def test_code_search(self):
+        embedding = Embedding()
+
+        code_fragments = [
+            Fragment('a/b/test.py',
+                     1,
+                     '''\
+class GMLExporter:
+
+    def __init__(self, model):...
+    def export(self, filepath):...
+''',
+                     'GMLExporter'),
+            Fragment('a/b/test.py',
+                     10,
+                     '''\
+class SomeOtherClass:
+
+    def __init__(self, model):...
+    def export(self, filepath):...
+''',
+                     'SomeOtherClass'),
+        ]
+
+        fragment_embeddings: np.ndarray = embedding.embed_fragments(code_fragments)
+
+        query_embeddings: np.ndarray = embedding.embed_query('class GMLExporter')
+        similarities: np.ndarray = cosine_similarity(query_embeddings, fragment_embeddings)
+        print(similarities)
+        fragment_index = np.argmax(similarities)
+        self.assertEqual(fragment_index, 0)
+
+        query_embeddings: np.ndarray = embedding.embed_query('.py class GMLExporter')
+        similarities: np.ndarray = cosine_similarity(query_embeddings, fragment_embeddings)
+        print(similarities)
+        fragment_index = np.argmax(similarities)
+        self.assertEqual(fragment_index, 0)
+
+        query_embeddings: np.ndarray = embedding.embed_query('.py class SomeOtherClass')
+        similarities: np.ndarray = cosine_similarity(query_embeddings, fragment_embeddings)
+        print(similarities)
+        fragment_index = np.argmax(similarities)
+        self.assertEqual(fragment_index, 1)
+
+        query_embeddings: np.ndarray = embedding.embed_query('.py export method')
+        similarities: np.ndarray = cosine_similarity(query_embeddings, fragment_embeddings)
+        print(similarities)
+        self.assertTrue(np.all(np.abs(similarities - np.average(similarities)) < 0.02))
+        self.assertTrue(np.all(similarities > 0.90))
+
+        query_embeddings: np.ndarray = embedding.embed_query('completely_different method')
+        similarities: np.ndarray = cosine_similarity(query_embeddings, fragment_embeddings)
+        print(similarities)
+        self.assertTrue(np.all(np.abs(similarities - np.average(similarities)) < 0.05))
+        self.assertTrue(np.all(similarities < 0.79))
