4386d950b874359e315eb6bd112dd44194c3ac80
Viktor Ferenczi <viktor@ferenczi.eu>
2023-05-26 23:15:42 +0200

    Fixes

--- src/common/server.py
+++ src/common/server.py
@@ -6,6 +6,7 @@ import quart
 
 def run_app(app: quart.Quart, *coros: Coroutine, **kws):
     loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     for coro in coros:
         loop.create_task(coro)
     app.run(loop=loop, **kws)
