751f66a15b1710bf16d2b549a31f99b6b79599f2
Viktor Ferenczi <viktor@ferenczi.eu>
2023-06-12 23:57:16 +0200

    Cleanup project CLI

---
+++ src/cleanup_project.py
@@ -0,0 +1,16 @@
+"""Deletes a project
+"""
+import asyncio
+import sys
+
+from project.project import Project
+
+
+async def main():
+    project_id = sys.argv[1]
+    project = Project(project_id)
+    await project.cleanup()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
