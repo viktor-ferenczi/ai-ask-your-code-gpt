27a256783aa0710f853f9a712004a85871da8fc7
Viktor Ferenczi <viktor@ferenczi.eu>
2023-06-12 14:35:42 +0200

    Fragment doc

--- src/model/fragment.py
+++ src/model/fragment.py
@@ -3,13 +3,13 @@ from dataclasses import dataclass
 
 @dataclass
 class Fragment:
-    uuid: str
-    path: str
-    lineno: int
-    depth: int
-    type: str
-    name: str
-    text: str
+    uuid: str  # Unique identifier of the fragment, UUID4
+    path: str  # Path of the source file
+    lineno: int  # Line number inside the source file
+    depth: int  # Depth in the logical structure of the code, 0 is the module global level
+    type: str  # Type of the code construct, for example: function, class, interface, method, field, variable, macro
+    name: str  # Name of the code construct
+    text: str  # Definition of the code construct in case of definitions, empty string in case of usage
 
     @staticmethod
     def clone(fragment: "Fragment") -> "Fragment":
