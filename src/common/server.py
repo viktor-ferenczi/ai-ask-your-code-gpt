80267ac238c35ba1f89e850e2ed8dd46acbe666f
Viktor Ferenczi <viktor@ferenczi.eu>
2023-06-27 20:01:40 +0200

    Simpler async run tool

--- src/common/server.py
+++ src/common/server.py
@@ -5,9 +5,8 @@ import quart
 
 
 async def run_app(app: quart.Quart, *coros: Coroutine, **kws):
-    tasks = [app.run_task(**kws)]
-    tasks.extend([asyncio.create_task(coro) for coro in coros])
-    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+    coros = coros + (app.run_task(**kws), )
+    done, pending = await asyncio.wait(coros, return_when=asyncio.FIRST_COMPLETED)
     for task in done:
         task.result()
     for task in pending:
