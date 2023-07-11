78e0db37bc38bf075773b14937e68b5e2ade68c0
Viktor Ferenczi <viktor@ferenczi.eu>
2023-07-12 01:05:03 +0200

    Test data

--- src/tests/test_backend_data/expected/small-tail-py-name.py
+++ src/tests/test_backend_data/expected/small-tail-py-name.py
@@ -26,6 +26,21 @@
           'file_list in files_by_size.items() if len(file_list) > 1)\n'
           '\n'
           '        # Calculate the hash only for groups with more than one '
-          'file\n',
-     tokens=166,
+          'file\n'
+          '        with ProcessPoolExecutor() as executor, '
+          "tqdm(total=total_size, unit='B', unit_scale=True, "
+          'desc="Processing files") as pbar:\n'
+          '            for file_size, file_list in files_by_size.items():\n'
+          '                if len(file_list) > 1:\n'
+          '                    file_checksums = '
+          'list(executor.map(md5_checksum, file_list))\n'
+          '                    for file_path, file_checksum in zip(file_list, '
+          'file_checksums):\n'
+          '                        '
+          'files_by_checksum[file_checksum].append(file_path)\n'
+          '                        pbar.update(file_size)\n'
+          '\n'
+          '        self.files.extend(file_list for file_list in '
+          'files_by_checksum.values() if len(file_list) > 1)',
+     tokens=295,
      score=1.0)]
