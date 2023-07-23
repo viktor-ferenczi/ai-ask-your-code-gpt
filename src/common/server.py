07bbda8fa095d649d3e074f6f8cfcdeafd24ec0e
Viktor Ferenczi <viktor@ferenczi.eu>
2023-07-24 00:45:58 +0200

    Extra startup log

--- src/common/server.py
+++ src/common/server.py
@@ -1,10 +1,12 @@
 import asyncio
+from datetime import datetime
 from typing import Coroutine
 
 import quart
 
 
 async def run_app(app: quart.Quart, *coros: Coroutine, **kws):
+    print(f'{datetime.utcnow().isoformat()}: Service started')
     coros = coros + (app.run_task(**kws), )
     done, pending = await asyncio.wait(coros, return_when=asyncio.FIRST_COMPLETED)
     for task in done:
